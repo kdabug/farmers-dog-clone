f0cbdf4d2fb27b77f243043768a194fd5e8b7efa
Mikayda Mills <mills.ma12@gmail.com>
2019-04-30 19:28:27 -0400

    theme passes through props to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import styled from "styled-components";
+
+export const Container = styled.section`
+  color: ${props => props.theme.salmon};
+`;
 
 export const App = props => {
   console.log(props);
-  return <div>hello world</div>;
+  return <Container>hello world</Container>;
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 import { App } from "./components/App";
-import { theme } from "./theme";
 
+console.log(theme);
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <App />
