03f864b7e50f1bfd29a8c458985b7758c6af0a85
Mikayda Mills <mills.ma12@gmail.com>
2019-04-30 19:21:59 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const App = props => {
+  console.log(props);
+  return <div>hello world</div>;
+};
---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+export const theme = {
+  grey: "#4B4F54",
+  white: "#ffffff",
+  salmon: "#ff8d6a",
+  primaryFont: "Muli"
+};
+
+//#4B4F54;
+//#ffffff;
+//#ff8d6a;
+//"Harmonia Sans", Helvetica, Arial, sans-serif;
